e58c9e3d8af6507cac65b1663ddfd7c156fc4c59
kylest0kes <kylestokes1551@gmail.com>
2020-12-07 19:38:27 -0500

    added transition on button press to add and remove classes to add effect

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 window.addEventListener('keydown', (e) => {
-//    console.log(e);
-   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//    console.log(audio);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const control = document.querySelector(`.control[data-key="${e.keyCode}"]`);
+
     if(!audio) return;
+    
     audio.currentTime = 0;
     audio.play();
-})
+
+    control.classList.add('pressed');
+});
+
+const controls = document.querySelectorAll('.control');
+
+controls.forEach(control => control.addEventListener('transitionend', removeTransition));
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+
+    this.classList.remove('pressed');
+}
